3f13eea7a17fef6dc61d088995df0de5af3e1f46
Trina <92634994+trinayau@users.noreply.github.com>
2022-11-08 22:36:04 +0000

    add react to project

---
+++ react-frontend/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      
+        <p>
+          CarbonCompare
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
