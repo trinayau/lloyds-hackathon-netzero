e8a45b195c5cf247c63dfb53fd5bac3462a8e5df
Trina <92634994+trinayau@users.noreply.github.com>
2022-11-10 22:30:12 +0000

    change button on homepage

--- react-frontend/src/components/Footer/index.js
+++ react-frontend/src/components/Footer/index.js
@@ -2,7 +2,7 @@ import MailOutlineIcon from '@mui/icons-material/MailOutline';
 import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
 const Footer = () => {
     return ( <footer>
-        <ul className="list-unstyled d-flex justify-content-around flex-wrap" >
+        <ul class="list-unstyled d-flex justify-content-around flex-wrap p-1 align-items-center" >
             <li class="font-weight-bold">Contact:</li>
             <li><LocalPhoneIcon fontSize='small'/> 07489384726</li>
             <li><MailOutlineIcon fontSize='small'/> <a class="text-decoration-none" href="mailto:contact@carbonaltdel.com">contact@carbonaltdel.com</a></li>
--- react-frontend/src/pages/HomePage/index.js
+++ react-frontend/src/pages/HomePage/index.js
@@ -10,7 +10,17 @@ const HomePage = () => {
     <div className="content" style={{justifyContent: 'center'}}>
         <h1>Helping Restaurants Go Net Zero</h1>
         <p>Order ingredients from your suppliers and offset your carbon footprint in just one click.</p>
-        <Button variant="contained" href="/"sx={{backgroundColor:'#354F52', my:'5px'}}>Join Today</Button>
+        <Button variant="contained" href="/login" 
+            sx={{backgroundColor:'#354F52',
+                 my:'5px',
+                 "&:hover": {
+                    backgroundColor: '#52796f',
+                    color: '#ffffff',
+                    textDecoration: 'none',
+                    transition: 'all 0.2s ease-in',
+                  },}}>
+            Get Started
+            </Button>
 
         </div>
     </div> );
