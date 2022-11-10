10f9ccd701144d94c24e6a951049d82a53683994
Trina <92634994+trinayau@users.noreply.github.com>
2022-11-10 22:24:23 +0000

    add text in front of video

--- react-frontend/src/components/Footer/index.js
+++ react-frontend/src/components/Footer/index.js
@@ -1,11 +1,11 @@
-import './index.css';
-
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
 const Footer = () => {
     return ( <footer>
-        <ul class="list-unstyled d-flex justify-content-around">
-            <li class="font-weight-bold">Contact Us:</li>
-            <li>Tel: 07489384726</li>
-            <li>Email: contact@carbonaltdel.com</li>
+        <ul className="list-unstyled d-flex justify-content-around flex-wrap" >
+            <li class="font-weight-bold">Contact:</li>
+            <li><LocalPhoneIcon fontSize='small'/> 07489384726</li>
+            <li><MailOutlineIcon fontSize='small'/> <a class="text-decoration-none" href="mailto:contact@carbonaltdel.com">contact@carbonaltdel.com</a></li>
         </ul>
     </footer> );
 }
--- react-frontend/src/pages/HomePage/index.js
+++ react-frontend/src/pages/HomePage/index.js
@@ -1,9 +1,16 @@
 import MyVideo from './openingvid.mp4';
+import './index.css';
 
 const HomePage = () => {
-    return ( <>
-    <video autostart autoPlay loop muted src={MyVideo} type="video/mp4" class="w-100 h-100 mh-vh"/>
-    </> );
+    return ( 
+    <div style={{display:'flex', position: 'relative', flexDirection: 'column'}}>
+
+        <video autostart autoPlay loop muted src={MyVideo} type="video/mp4" class="w-100 h-100 mh-vh"/>
+    <div className="content" style={{justifyContent: 'center'}}>
+        <h1>Helping Restaurants Go Net Zero</h1>
+        <p>Compare and track the carbon footprint of ingredients from all of your suppliers.</p>
+        </div>
+    </div> );
 }
  
 export default HomePage;
