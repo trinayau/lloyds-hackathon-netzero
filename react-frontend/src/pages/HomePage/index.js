4acd34f1cf55d2303d222b9227a046b28313bee1
Trina <92634994+trinayau@users.noreply.github.com>
2022-11-10 22:27:06 +0000

    add button to homepage

--- react-frontend/src/pages/HomePage/index.js
+++ react-frontend/src/pages/HomePage/index.js
@@ -1,5 +1,6 @@
 import MyVideo from './openingvid.mp4';
 import './index.css';
+import {Button} from '@mui/material'; 
 
 const HomePage = () => {
     return ( 
@@ -8,7 +9,9 @@ const HomePage = () => {
         <video autostart autoPlay loop muted src={MyVideo} type="video/mp4" class="w-100 h-100 mh-vh"/>
     <div className="content" style={{justifyContent: 'center'}}>
         <h1>Helping Restaurants Go Net Zero</h1>
-        <p>Compare and track the carbon footprint of ingredients from all of your suppliers.</p>
+        <p>Order ingredients from your suppliers and offset your carbon footprint in just one click.</p>
+        <Button variant="contained" href="/"sx={{backgroundColor:'#354F52', my:'5px'}}>Join Today</Button>
+
         </div>
     </div> );
 }
