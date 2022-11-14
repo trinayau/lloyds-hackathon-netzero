e5fe84998aac436865ae3eecab8a9e02f167adb9
Fuwa-Kei <116655254+Fuwa-Kei@users.noreply.github.com>
2022-11-14 14:32:40 +0000

    started accounts page

---
+++ react-frontend/pages/ResultsPage/index.js
@@ -0,0 +1,7 @@
+const ResultsPage = () => {
+    return ( <>
+    <h1>Search results page</h1>
+    </> );
+}
+ 
+export default ResultsPage;
---
+++ react-frontend/pages/index.js
@@ -0,0 +1,10 @@
+export {default as AccountPage} from './AccountPage';
+export {default as LoginPage} from './LoginPage';
+export {default as ResultsPage} from './ResultsPage';
+export {default as ShoppingCartPage} from './ShoppingCartPage';
+export {default as HomePage} from './HomePage';
+export {default as NotFoundPage} from './NotFoundPage';
+export {default as SingleProductPage} from './SingleProductPage';
+export {default as OrderStatusPage} from './OrderStatusPage';
+export {default as AllProductPage} from './AllProductPage';
+
